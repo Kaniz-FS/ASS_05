636b90c4664c31458b582fe1d10bbf70d9443c70
Kaniz-FS <ravenclaw.k12@gmail.com>
2025-08-30 16:34:44 +0600

    go

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,15 @@
 console.log("hello");
+
+document.getElementById("emergency-btn").addEventListener("click", function () {
+  const coins = getInnerTextById("coin-id");
+  //   console.log(coins);
+
+  const newCoin = coins - 20;
+
+  if (newCoin < 0) {
+    alert("stop the process");
+  } else {
+    setInnerTextByIDandValue("coin-id", newCoin);
+    alert("calling National Emergency Services 999..");
+  }
+});
